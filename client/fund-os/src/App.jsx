a05d1633a17a9fbe3160af52695d3a34ad0aa94f
Barrett Kowalsky <barrettkowalsky@gmail.com>
2024-01-30 12:11:33 -0700

    initialize repo

---
+++ client/fund-os/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import { Flex, Text, Button } from '@radix-ui/themes';
+// import './App.css'
+
+
+function App() {
+
+  return (
+    
+    
+      <>
+       
+        <Flex direction="column" gap="2">
+          <Text>Hello from Radix Themes :)</Text>
+          <Button>Let's go</Button>
+        </Flex>
+      </>
+  
+  )
+}
+
+export default App
