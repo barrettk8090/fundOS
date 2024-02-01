43ba69504de08464f3935f50b38cead8a80f9c7b
Barrett Kowalsky <barrettkowalsky@gmail.com>
2024-01-31 19:18:20 -0700

    add final single project props to project card

--- client/fund-os/src/components/ProjectCard.jsx
+++ client/fund-os/src/components/ProjectCard.jsx
@@ -8,12 +8,12 @@ function ProjectCard({singleProject}){
     return (
         <div className="project-card">
             <img src="https://via.placeholder.com/150" alt="Project Image"/>
-            <h2>Project Title: {singleProject.name}</h2>
-            <p>Project Description: </p>
-            <p>Project Creator</p>
-            <p>Project Goal</p>
-            <p>Project Current Amount</p>
-            <p>Project End Date</p>
+            <h2>{singleProject.name}</h2>
+            <p>{singleProject.description}</p>
+            <p>USER ID:{singleProject.user_id}</p>
+            <p>Funds needed:{singleProject.funding_needed}</p>
+            <p>Current Project Amount: {singleProject.current_funding}</p>
+            <p>Deadline:{singleProject.deadline}</p>
             <button>Explore Project →</button>
         </div>
     )
