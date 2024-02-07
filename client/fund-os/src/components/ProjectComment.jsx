903745fb754253909118da26afb89b271d3eed5e
Barrett Kowalsky <barrettkowalsky@gmail.com>
2024-02-07 14:19:59 -0700

    add user comments

--- client/fund-os/src/components/ProjectComment.jsx
+++ client/fund-os/src/components/ProjectComment.jsx
@@ -4,6 +4,8 @@ function ProjectComment({comment}){
         <div>
             <p>Comment: {comment.comment_text}</p>
             <p>User: {comment.user.username}</p>
+            <img className="w-10 pb-6 rounded-full" src={comment.user.image}></img>
+            <hr/>
         </div>
     )
 }
